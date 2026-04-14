d3e20de1ff86dcd8e49905d75059a118009fc0f9
Maicon Strey <mstrey@gmail.com>
2026-04-14 15:04:02 -0300

    ajuste no scritp de desativação dos containers

--- scripts/ups_manager.sh
+++ scripts/ups_manager.sh
@@ -43,23 +43,25 @@ while true; do
     CHARGE=$(upsc $UPS_NAME battery.charge 2>/dev/null)
     STATUS=$(upsc $UPS_NAME ups.status 2>/dev/null)
 
+    echo "$(date) - Bateria com $CHARGE% de carga e status $STATUS."
+    # Aguarda 60 segundos até a próxima checagem
+    sleep 60
     if [ ! -z "$CHARGE" ] && [ ! -z "$STATUS" ]; then
-        
+        if [ "$charge" -gt 55 ]; then
+            continue
+        fi        
         if nobreak_sem_energia "$STATUS" "$CHARGE"; then
             echo "$(date) - Energia ausente. Bateria em $CHARGE%. Parando containers..."
             # Ignora o NUT e este próprio container gerenciador
-            docker ps --format "{{.Names}}" | grep -vE "^(${NUT_CONTAINER}|${MANAGER_CONTAINER})$" | xargs -r docker stop
+            #docker ps --format "{{.Names}}" | grep -vE "^(${NUT_CONTAINER}|${MANAGER_CONTAINER})$" | xargs -r docker stop
         fi    
 
         if nobreak_com_energia "$STATUS" "$CHARGE"; then
             PARADOS=$(docker ps -a -f "status=exited" --format "{{.Names}}")
             if [ ! -z "$PARADOS" ]; then
                 echo "$(date) - Energia restaurada. Bateria segura em $CHARGE%. Iniciando containers..."
-                docker ps -a -f "status=exited" --format "{{.Names}}" | xargs -r docker start
+                #docker ps -a -f "status=exited" --format "{{.Names}}" | xargs -r docker start
             fi
         fi
     fi
-    echo "$(date) - Energia presente. Bateria em $CHARGE%."
-    # Aguarda 60 segundos até a próxima checagem
-    sleep 60
 done
